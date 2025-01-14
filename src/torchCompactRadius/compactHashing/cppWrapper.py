0c03f7743a2063e8d5a6511592bfa68cff3a98c4
Rene <rene.winchenbach@gmail.com>
2025-01-14 14:24:01 +0100

    fixed import issue

--- src/torchCompactRadius/compactHashing/cppWrapper.py
+++ src/torchCompactRadius/compactHashing/cppWrapper.py
@@ -14,6 +14,7 @@ try:
         warnings.warn('Precompiled GPU version of the neighbor search is being used even though CUDA is not available!')
     hasPrecompiledGPU = True
 except ImportError:
+    # warnings.warn('No cuda version of the neighbor search is available.')
     pass
 if not hasPrecompiledGPU:
     try:
@@ -23,21 +24,29 @@ if not hasPrecompiledGPU:
             warnings.warn('Precompiled CPU version of the neighbor search is being used even though CUDA is available!')
         hasPrecompiledCPU = True
     except ImportError:
+        # warnings.warn('No cpu version of the neighbor search is available.')
         pass
 
 if not hasPrecompiledCPU and not hasPrecompiledGPU:
     warnings.warn('No precompiled version of the neighbor search is available.')
     neighborSearch_cpp = compileSourceFiles(
-        ['cppSrc/neighborhoodDynamic.cpp', 'cppSrc/neighborhoodDynamic.cu', 
-        'cppSrc/neighborhoodFixed.cpp', 'cppSrc/neighborhoodFixed.cu',
-        'cppSrc/hashing.cpp', 'cppSrc/hashing.cu',
-        'cppSrc/neighborhoodSmall.cpp', 'cppSrc/neighborhoodSmall.cu',
+        ['cppSrc/neighborhoodDynamic_cpu.cpp',  'cppSrc/neighborhoodDynamic_cuda.cu', 
+        'cppSrc/neighborhoodFixed_cpu.cpp',     'cppSrc/neighborhoodFixed_cuda.cu',
+        'cppSrc/hashing_cpu.cpp',               'cppSrc/hashing_cuda.cu',
+        'cppSrc/neighborhoodSmall_cpu.cpp',     'cppSrc/neighborhoodSmall_cuda.cu',
         'cppSrc/cppWrapper.cpp'], module_name = 'torchCompactRadius_jit', verbose = False, openMP = True, verboseCuda = False, cuda_arch = None)
 
-countNeighbors_cpp = neighborSearch_cpp.countNeighbors
-buildNeighborList_cpp = neighborSearch_cpp.buildNeighborList
-countNeighborsFixed_cpp = neighborSearch_cpp.countNeighborsFixed
-buildNeighborListFixed_cpp = neighborSearch_cpp.buildNeighborListFixed
+# countNeighbors_cpp = neighborSearch_cpp.countNeighbors
+# buildNeighborList_cpp = neighborSearch_cpp.buildNeighborList
+# countNeighborsFixed_cpp = neighborSearch_cpp.countNeighborsFixed
+# buildNeighborListFixed_cpp = neighborSearch_cpp.buildNeighborListFixed
 hashCells_cpp = neighborSearch_cpp.computeHashIndices
-neighborSearchSmall = neighborSearch_cpp.neighborSearchSmall
-neighborSearchSmallFixed = neighborSearch_cpp.neighborSearchSmallFixed
+# neighborSearchSmall = neighborSearch_cpp.neighborSearchSmall
+# neighborSearchSmallFixed = neighborSearch_cpp.neighborSearchSmallFixed
+
+countNeighbors_cpp = None
+buildNeighborList_cpp = None
+countNeighborsFixed_cpp = None
+buildNeighborListFixed_cpp = None
+neighborSearchSmall = None
+neighborSearchSmallFixed = None
--- src/torchCompactRadius/cppSrc/cppWrapper.cpp
+++ src/torchCompactRadius/cppSrc/cppWrapper.cpp
@@ -5,14 +5,14 @@
 
 // Create the python bindings for the C++ functions
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("countNeighbors", &countNeighbors, "Count the Number of Neighbors (C++) using a precomputed hash table and cell map");
-  m.def("buildNeighborList", &buildNeighborList, "Build the Neighborlist (C++) using a precomputed hash table and cell map as well as neighbor counts");
+  // m.def("countNeighbors", &countNeighbors, "Count the Number of Neighbors (C++) using a precomputed hash table and cell map");
+  // m.def("buildNeighborList", &buildNeighborList, "Build the Neighborlist (C++) using a precomputed hash table and cell map as well as neighbor counts");
     
-  m.def("countNeighborsFixed", &countNeighborsFixed, "Count the Number of Neighbors (C++) using a precomputed hash table and cell map (fixed support radius)");
-  m.def("buildNeighborListFixed", &buildNeighborListFixed, "Build the Neighborlist (C++) using a precomputed hash table and cell map as well as neighbor counts (fixed support radius)");
+  // m.def("countNeighborsFixed", &countNeighborsFixed, "Count the Number of Neighbors (C++) using a precomputed hash table and cell map (fixed support radius)");
+  // m.def("buildNeighborListFixed", &buildNeighborListFixed, "Build the Neighborlist (C++) using a precomputed hash table and cell map as well as neighbor counts (fixed support radius)");
 
   m.def("computeHashIndices", &computeHashIndices, "Compute the Hash Indices (C++)");
 
-  m.def("neighborSearchSmall", &neighborSearchSmall, "Neighbor Search (C++)");
-  m.def("neighborSearchSmallFixed", &neighborSearchSmallFixed, "Neighbor Search (C++) (fixed support radius)");
+  // m.def("neighborSearchSmall", &neighborSearchSmall, "Neighbor Search (C++)");
+  // m.def("neighborSearchSmallFixed", &neighborSearchSmallFixed, "Neighbor Search (C++) (fixed support radius)");
 }
