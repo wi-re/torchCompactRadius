2cb8620c3c998df058bd36b863b7bb868e0f50a1
Rene <rene.winchenbach@gmail.com>
2025-01-29 10:55:25 +0100

    update prebuilts

--- build2.sh
+++ build2.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# ./build.sh 2.5.0 cu124
+./build.sh 2.5.0 cu124
 ./build.sh 2.5.1 cu124
 
-# ./build.sh 2.5.0 cu121
-# ./build.sh 2.5.1 cu121
+./build.sh 2.5.0 cu121
+./build.sh 2.5.1 cu121
 
-# ./build.sh 2.5.0 cu118
-# ./build.sh 2.5.1 cu118
+./build.sh 2.5.0 cu118
+./build.sh 2.5.1 cu118
 
-# ./build.sh 2.5.0 cpu
-# ./build.sh 2.5.1 cpu
+./build.sh 2.5.0 cpu
+./build.sh 2.5.1 cpu
+
+git restore setup.py
+git restore src/torchCompactRadius/__init__.py
+
+python genhtmls.py
+
+git add .
+git commit -m "update prebuilts"
+git push
